5f2c80e7011e3277909b322b0c6d41cfc63215a7
askiiart <90910526+askiiart@users.noreply.github.com>
2023-08-29 21:52:55 +0000

    Fix pip3 install (fr this time)

--- testing/run.sh
+++ testing/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 git clone https://github.com/7trail/AlwaysDungeons
-pip3 install -r data/AlwaysDungeons/requirements
+pip3 install -r /data/AlwaysDungeons/requirements.txt
 cp /data/.env /data/AlwaysDungeons/.env
 cd /data/AlwaysDungeons
 python3 main.py
