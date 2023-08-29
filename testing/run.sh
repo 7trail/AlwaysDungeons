f1bcaffa4a1d0511d105f13b8c2b18110606bb2b
askiiart <dev@askiiart.net>
2023-08-29 15:36:23 -0500

    Fix run.sh

--- testing/run.sh
+++ testing/run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
-cd /data
 git clone https://github.com/7trail/AlwaysDungeons
-cd /data/AlwaysDungeons
-pip3 install -r requirements
+pip3 install -r data/AlwaysDungeons/requirements
 cp /data/.env /data/AlwaysDungeons/.env
+cd /data/AlwaysDungeons
 python3 main.py
