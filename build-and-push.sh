a68832f68c0aee0b326d041257e08a53e8d13b00
askiiart <dev@askiiart.net>
2023-09-06 15:01:08 -0500

    Re-add docker patch

--- build-and-push.sh
+++ build-and-push.sh
@@ -4,11 +4,12 @@ ORG="askiiart"
 NAME="discord-always-dungeons"
 
 # Apply patch for Docker
-docker run -v .:/repo python /bin/sh -c "cd /repo && pip install -r requirements.txt && apt-get update && apt-get install binutils -y && pip install pyinstaller && pyinstaller main.py && cd /repo/dist/main && chmod -R 777 /repo/dist/main && tar cvfz /repo/AlwaysDungeons.tar.gz *"
+git apply docker.patch
 
 # Build image
-#ID=$(docker build . -q)
+docker run -v .:/repo python /bin/sh -c "cd /repo && pip install -r requirements.txt && apt-get update && apt-get install binutils -y && pip install pyinstaller && pyinstaller main.py && cd /repo/dist/main && chmod -R 777 /repo/dist/main && tar cvfz /repo/AlwaysDungeons.tar.gz *"
+ID=$(docker build . -q)
 
 # Could just do -t on the build, but this makes it easier to expand if needed.
-#docker tag ${ID} ${ORG}/${NAME}:latest
-#docker push ${ORG}/${NAME}:latest
+docker tag ${ID} ${ORG}/${NAME}:latest
+docker push ${ORG}/${NAME}:latest
