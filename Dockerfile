5e34e60a97988e5d6a3b816478381e15f1a2f084
askiiart <dev@askiiart.net>
2023-10-31 18:15:57 -0500

    Fix missing rustc

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM python:slim-bookworm
 WORKDIR /data
+RUN apt update && apt install -y rust-all
 ADD AlwaysDungeons.tar.gz /data
 CMD ["/data/main"]
--- build.sh
+++ build.sh
@@ -5,15 +5,16 @@ ORG="askiiart"
 NAME="discord-always-dungeons"
 PUSH="false"
 
+
 # Apply patch for Docker
 git apply docker.patch
 
 # Build image
-docker run -v .:/repo python /bin/sh -c "cd /repo && pip install -r requirements.txt && apt-get update && apt-get install binutils -y && pip install pyinstaller && pyinstaller main.py && cd /repo/dist/main && chmod -R 777 /repo/dist/main && tar cvfz /repo/AlwaysDungeons.tar.gz *"
+docker run -v .:/repo python /bin/sh -c "apt update && apt install -y rustc && cd /repo && pip install -r requirements.txt && apt-get update && apt-get install binutils -y && pip install pyinstaller && pyinstaller main.py && cd /repo/dist/main && chmod -R 777 /repo/dist/main && tar cvfz /repo/AlwaysDungeons.tar.gz *"
 ID=$(docker build . -q)
 
 # Could just do -t on the build, but this makes it easier to expand if needed.
-if [ $ID == "true" ]; then
+if [ $ID == "true" && $PUSH="true" ]; then
     docker tag ${ID} ${ORG}/${NAME}:latest
     docker push ${ORG}/${NAME}:latest
 else
